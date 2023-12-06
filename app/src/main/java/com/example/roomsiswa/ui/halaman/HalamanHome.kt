25841b582aabd91bc0b5a69557d2b10cb32ab80d
roqisb <roqisb9@gmail.com>
2023-12-06 20:54:47 +0700

    add row and text nama

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -2,9 +2,13 @@ package com.example.roomsiswa.ui.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -30,7 +34,14 @@ fun DataSiswa(
             modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
         ){
-
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(
+                text = siswa.nama,
+                style = MaterialTheme.typography.titleLarge,
+                )
+        }
         }
     }
 }
