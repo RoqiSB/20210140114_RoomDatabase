d8c0ec95b16a7ca8bd99e04740416ea636d4fde6
roqisb <roqisb9@gmail.com>
2023-12-06 20:48:13 +0700

    add object DestinasiHome

---
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomsiswa.ui.halaman
+
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    val route = "home"
+    val titleRes = R.string.app_name
+}
