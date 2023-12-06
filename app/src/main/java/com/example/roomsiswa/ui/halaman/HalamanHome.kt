1643652c0f6fb488608758e8e68def815a9ac40b
roqisb <roqisb9@gmail.com>
2023-12-06 20:59:24 +0700

    add another text and add icons phone

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -3,10 +3,14 @@ package com.example.roomsiswa.ui.halaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,7 +45,20 @@ fun DataSiswa(
                 text = siswa.nama,
                 style = MaterialTheme.typography.titleLarge,
                 )
+            Spacer(Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.Phone,
+                contentDescription = null,
+            )
+            Text(
+                text = siswa.telpon,
+                style = MaterialTheme.typography.titleMedium,
+            )
         }
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium,
+            )
         }
     }
 }
