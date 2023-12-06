92459647f96f63317d1d44aa3ea5450f6795b4d5
roqisb <roqisb9@gmail.com>
2023-12-06 17:41:29 +0700

    add package for viewmodel and add entryviewmodel

---
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.roomsiswa.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi Status Siswa Saat ini
+     **/
+    var UiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /** Fungsi untuk memvalidasi input*/
+
+    private fun validasiInput(uiState: DetailSiswa = UiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        UiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(UiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+data class DetailSiswa(
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telpon : String = ""
+)
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
